e62e35b14709a885288bb319bd7f30ca6aa9c5cb
Mustafa <eczmustafaozturk@gmail.com>
2022-06-08 14:19:18 +0300

    db engine fix

--- mozio/settings/prod.py
+++ mozio/settings/prod.py
@@ -4,10 +4,11 @@ import dj_database_url
 
 DEBUG = False
 
-SECRET_KEY = os.environ["SECRET_KEY"]
+SECRET_KEY = os.environ['SECRET_KEY']
 
-ALLOWED_HOSTS = ["ec2-3-73-79-152.eu-central-1.compute.amazonaws.com"]
+ALLOWED_HOSTS = ['ec2-3-73-79-152.eu-central-1.compute.amazonaws.com']
 
 DATABASES = {
     'default': dj_database_url.config(),
 }
+DATABASES['default']['ENGINE': 'django.contrib.gis.db.backends.postgis',]
