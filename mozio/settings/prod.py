6d264ecb61e3b5028d46d80f3c09847302fdabb8
Mustafa <eczmustafaozturk@gmail.com>
2022-06-08 13:28:06 +0300

    allowed hosts updated

--- mozio/settings/prod.py
+++ mozio/settings/prod.py
@@ -6,7 +6,7 @@ DEBUG = False
 
 SECRET_KEY = os.environ["SECRET_KEY"]
 
-ALLOWED_HOSTS = ["x.aws.com"]
+ALLOWED_HOSTS = ["ec2-3-73-79-152.eu-central-1.compute.amazonaws.com"]
 
 DATABASES = {
     'default': dj_database_url.config(),
