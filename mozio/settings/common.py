8e5d34239d16b8f147d478291c001451309dafda
Mustafa <eczmustafaozturk@gmail.com>
2022-06-09 11:58:50 +0300

    added markdown dependency and pagination

--- mozio/settings/common.py
+++ mozio/settings/common.py
@@ -116,3 +116,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static")
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'COERCE_DECIMAL_TO_STRING':False,
+    'DEFAULT_PAGINATION_CLASS':'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE':10,
+}
